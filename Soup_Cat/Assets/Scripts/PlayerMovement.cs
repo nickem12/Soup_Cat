5438b149bcd7dc239c182fd55c800ed2734d4d3f
nickem12 <nick_comeau3@hotmail.com>
2016-10-27 11:34:11 -0300

    Soup Cat Movement
    BAsic buggy movement has been added

---
+++ Soup_Cat/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    //Movement
+
+    public float speed;
+    public float jump;
+    float velocity;
+
+    int jumpCounter = 0;
+
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.Space)|| Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if(jumpCounter == 0)
+            {
+                jumpCounter++;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+            }
+            
+        }
+
+        velocity = 0;
+
+        if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
+        {
+            velocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            velocity = speed;
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(velocity, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void OnTriggerEnter2D()
+    {
+        jumpCounter = 0;
+    }
+}
