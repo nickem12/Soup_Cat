a853467c4dd724b61f170793339d2307588884f5
nickem12 <nick_comeau3@hotmail.com>
2016-11-05 12:06:10 -0300

    Soup Cat stuff

---
+++ Soup_Cat/Assets/Scripts/soupDrop.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soupDrop : MonoBehaviour {
+
+    float soupSpeed = 10;
+    float dir;
+    private GameObject player;
+
+    private int counter = 20;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        dir = -player.transform.localScale.x;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        counter--;
+        if(counter<=0)
+        {
+            Destroy(gameObject);
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(dir * soupSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+	}
+}
