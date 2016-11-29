c0f7631a51aa558d166efee972a7009991aff9b4
pow752 <charlied06@live.com>
2016-11-29 12:36:21 -0400

    -cat
    fuck you

---
+++ Soup_Cat/Assets/Scripts/Ui/Health.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    [SerializeField]
+    private float Healthremaining;
+
+    [SerializeField]
+    private Image helth;
+
+    public float MaxValyou { get; set; }
+
+    public float valyou
+    {
+        set
+        {
+            Healthremaining = HelthMath(valyou, 0, MaxValyou, 0, 1);
+        }
+
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+
+	// Update is called once per frame
+	void Update ()
+    {
+        HelthBarStuff();
+    }
+
+
+    private void HelthBarStuff()
+    {
+
+        helth.fillAmount = Healthremaining;
+
+    } 
+
+    private float HelthMath(float valyou, float inMin, float inMax, float outMax, float outMin)
+    {
+
+        return ((valyou - inMin) * (outMax - outMin) / (inMax - inMin) + outMin);
+
+    }
+}
