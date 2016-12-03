055c7645f31ea8a76696519c5ac8bb88bc3714ff
spartem <nidoucette@hotmail.com>
2016-12-03 13:29:38 -0400

    k

---
+++ Soup_Cat/Assets/Scripts/AI/Nawhal.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nawhal : MonoBehaviour {
+
+    float distance;
+    float velocity;
+    GameObject player;
+    float YMax;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindWithTag("Player");
+        YMax = transform.position.y + 4;
+        velocity = 10;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+	    if (distance < 5)
+        {
+            if (transform.position.y < YMax)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, velocity);
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+            }
+        }
+	}
+}
